88abeb50358f6bee11da3d71acf5646a26abeb41
AdamRate <Adamjrate@hotmail.co.uk>
2017-09-07 15:44:53 +0000

    New Developer Branch

---
+++ src/main/webapp/app/feature/blog/blogController.js
@@ -0,0 +1,8 @@
+(function() {
+
+    var blogController =  function() {
+        var vm = this;
+    };
+
+    angular.module('cinema').controller('blogController', [blogController]);
+}());
---
+++ src/main/webapp/app/feature/blog2/blog2Controller.js
@@ -0,0 +1,8 @@
+(function() {
+
+    var blog2Controller =  function() {
+        var vm = this;
+    };
+
+    angular.module('cinema').controller('blog2Controller', [blog2Controller]);
+}());
