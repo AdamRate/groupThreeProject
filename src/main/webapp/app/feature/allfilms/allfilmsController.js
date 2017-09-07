418ef509e19e5e3d76dca0af9afe34ac6df3683b
AdamRate <Adamjrate@hotmail.co.uk>
2017-09-07 15:30:43 +0000

    Angular Conversion

---
+++ src/main/webapp/app/feature/allfilms/allfilmsController.js
@@ -0,0 +1,8 @@
+(function() {
+
+    var aFilmsController =  function() {
+        var vm = this;
+    };
+
+    angular.module('cinema').controller('aFilmsController', [aFilmsController]);
+}());
---
+++ src/main/webapp/app/feature/contact/contactController.js
@@ -0,0 +1,8 @@
+(function() {
+
+    var contactController =  function() {
+        var vm = this;
+    };
+
+    angular.module('cinema').controller('contactController', [contactController]);
+}());
