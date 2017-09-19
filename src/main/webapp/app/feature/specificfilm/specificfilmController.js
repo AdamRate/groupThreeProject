bd49c885d2a31172efda677bb6e14edb2ab4a659
Unknown <MorickClive@Gmail.com>
2017-09-19 11:56:06 +0100

    Push: functional async film listing
    Works now - no clunky implementation.

--- src/main/webapp/app/feature/specificfilm/specificfilmController.js
+++ src/main/webapp/app/feature/specificfilm/specificfilmController.js
@@ -2,35 +2,35 @@
 
     var sFController =  function(filmLoader, $state, $sce) {
         var vm = this;
+        vm.filmDat = [];
 
-        vm.filmDat = filmLoader['filmDat'][filmLoader['filmNo']];
+        // load film data
+        var promise = filmLoader['getFilms']();
+        promise.then(function (data) {
+            ////
+            // When film data loaded, perform this.
+            ////
 
-        // redirect to somewhere more meaningful
-        if(vm.filmDat === undefined){
-            console.error("No information for client to read - redirecting to film listings");
-            $state.go('allfilms');
-        }
-        else {
-            // generate cast List
+            // load our specific data by movie value stored in service.
+            vm.filmDat = data[filmLoader['filmNo']];
+            //format array string to singular string.
             vm.castList = (function () {
-                    var str = "";
-                    for (var x = 0; x < vm.filmDat['castList'].length; x++) {
-                        if (x === vm.filmDat['castList'].length - 1) {
-                            str += vm.filmDat['castList'][x] + ".";
-                        }
-                        else {
-                            str += vm.filmDat['castList'][x] + ", ";
-                        }
+                var str = "";
+                for (var x = 0; x < vm.filmDat['castList'].length; x++) {
+                    if (x === vm.filmDat['castList'].length - 1) {
+                        str += vm.filmDat['castList'][x] + ".";
                     }
-                    return str;
-                })();
-
+                    else {
+                        str += vm.filmDat['castList'][x] + ", ";
+                    }
+                }
+                return str;
+            })();
+            // consider security for videos
             vm.videoEmbed = $sce.trustAsResourceUrl(vm.filmDat["trailer"]);
-
-        }
+        });
 
     };
 
     angular.module("cinema").controller("sFController", ['filmLoader', '$state', '$sce', sFController]);
-
 }());
