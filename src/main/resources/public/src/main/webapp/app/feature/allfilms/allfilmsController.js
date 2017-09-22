79e818c687f84b899fcb96774bb1acb218e7965e
MaryOpenshaw <janeopenshaw@gmail.com>
2017-09-22 14:57:33 +0000

    more house keeping

--- src/main/resources/public/src/main/webapp/app/feature/allfilms/allfilmsController.js
+++ src/main/resources/public/src/main/webapp/app/feature/allfilms/allfilmsController.js
@@ -3,26 +3,13 @@
     var aFilmsController =  function($state, filmLoader) {
         var vm = this;
         vm.filmList = [];
-        vm.reverse = false;
+
         var promise = filmLoader['getFilms']();
         promise.then(function (data) {
             vm.filmList = data;
         });
 
-       /* vm.navigate = function (movieNum) { filmLoader['navigate'](movieNum) };
-
-
-        vm.doSortBy= function(atribute) {
-        vm.doSortBy = atribute;
-        vm.reverse= !vm.reverse;
-
-
-        }*/
-
-
-
-
-
+        vm.navigate = function (movieNum) { filmLoader['navigate'](movieNum) };
     }
 
         angular.module('cinema').controller('aFilmsController', ['$state', 'filmLoader', aFilmsController]);
