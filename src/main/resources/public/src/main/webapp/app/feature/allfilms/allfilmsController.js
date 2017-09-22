27b087104fd3a9ae5f8b996481c5a33f429a58ec
MaryOpenshaw <janeopenshaw@gmail.com>
2017-09-22 12:58:38 +0000

    house keeping fixed image path from promo page

--- src/main/resources/public/src/main/webapp/app/feature/allfilms/allfilmsController.js
+++ src/main/resources/public/src/main/webapp/app/feature/allfilms/allfilmsController.js
@@ -3,13 +3,26 @@
     var aFilmsController =  function($state, filmLoader) {
         var vm = this;
         vm.filmList = [];
-
+        vm.reverse = false;
         var promise = filmLoader['getFilms']();
         promise.then(function (data) {
             vm.filmList = data;
         });
 
-        vm.navigate = function (movieNum) { filmLoader['navigate'](movieNum) };
+       /* vm.navigate = function (movieNum) { filmLoader['navigate'](movieNum) };
+
+
+        vm.doSortBy= function(atribute) {
+        vm.doSortBy = atribute;
+        vm.reverse= !vm.reverse;
+
+
+        }*/
+
+
+
+
+
     }
 
         angular.module('cinema').controller('aFilmsController', ['$state', 'filmLoader', aFilmsController]);
