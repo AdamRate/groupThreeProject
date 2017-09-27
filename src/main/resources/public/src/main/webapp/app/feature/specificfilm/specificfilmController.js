60e7c6eaead4bd405ca97566ecea7c012d610107
AdamRate <11336700@students.lincoln.ac.uk>
2017-09-27 07:57:51 +0000

    Update specificfilmController.js

--- src/main/resources/public/src/main/webapp/app/feature/specificfilm/specificfilmController.js
+++ src/main/resources/public/src/main/webapp/app/feature/specificfilm/specificfilmController.js
@@ -5,23 +5,23 @@
         vm.filmDat = [];
 
         // load film data
-        var promise = filmLoader['getFilms']();
+        var promise = filmLoader["getFilms"]();
         promise.then(function (data) {
             ////
             // When film data loaded, perform this.
             ////
 
             // load our specific data by movie value stored in service.
-            vm.filmDat = data[filmLoader['filmNo']];
+            vm.filmDat = data[filmLoader["filmNo"]];
             //format array string to singular string.
             vm.castList = (function () {
                 var str = "";
-                for (var x = 0; x < vm.filmDat['castList'].length; x++) {
-                    if (x === vm.filmDat['castList'].length - 1) {
-                        str += vm.filmDat['castList'][x] + ".";
+                for (var x = 0; x < vm.filmDat["castList"].length; x++) {
+                    if (x === vm.filmDat["castList"].length - 1) {
+                        str += vm.filmDat["castList"][x] + ".";
                     }
                     else {
-                        str += vm.filmDat['castList'][x] + ", ";
+                        str += vm.filmDat["castList"][x] + ", ";
                     }
                 }
                 return str;
@@ -33,5 +33,5 @@
         document.body.scrollTop = document.documentElement.scrollTop = 0;
     };
 
-    angular.module("cinema").controller("sFController", ['filmLoader', '$state', '$sce', sFController]);
+    angular.module("cinema").controller("sFController", ["filmLoader", "$state", "$sce", sFController]);
 }());
