b56f5fc6a11632f1c1c3b9c58f394dc903870e7e
AdamRate <11336700@students.lincoln.ac.uk>
2017-09-27 07:49:44 +0000

    Update contactController.js

--- src/main/resources/public/src/main/webapp/app/feature/contact/contactController.js
+++ src/main/resources/public/src/main/webapp/app/feature/contact/contactController.js
@@ -4,5 +4,5 @@
         var vm = this;
     };
 
-    angular.module('cinema').controller('contactController', [contactController]);
+    angular.module("cinema").controller("contactController", [contactController]);
 }());
