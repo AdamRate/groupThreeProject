c6c4adb786bea2f1d28a3e236139091a7e817014
nichharp <nichharp@hotmail.co.uk>
2017-09-28 09:12:04 +0100

    comments removed

--- src/main/resources/public/src/main/webapp/app/feature/signin/SignInController.js
+++ src/main/resources/public/src/main/webapp/app/feature/signin/SignInController.js
@@ -5,7 +5,6 @@
         var vm = this;
 
         vm.showRegistration = function () {
-            console.log("The 'show registration' button has been activated.");
             document.getElementById('register').style.display = "inline-block";
             document.getElementById('signIn').style.display = "none";
             event.preventDefault();
@@ -13,10 +12,8 @@
 
         vm.register = function (username,name, age, phone, email, password1, password2){
 
-            // console.log("INITAL"+ username + name + age );
             if (password1 !== password2){
                 alert("Passwords do not match");
-                console.log("PASSWORDS DO NOT MATCH");
             }
             else{
                 signInService.makeNewUser(username,name, email, age, phone, password1);
@@ -26,14 +23,12 @@
         };
 
         vm.signIn = function (email, password){
-            console.log("The 'sign in' button has been activated.");
             signInService.attemptToSignIn(email, password);
 
             return false;
         };
 
         vm.showForgot = function () {
-            console.log("The 'show Forgot' button has been activated.");
             document.getElementById('forgot').style.display = "inline-block";
             document.getElementById('signIn').style.display = "none";
             event.preventDefault();
@@ -41,18 +36,7 @@
 
         // this appears to need remind details
         vm.forgotDetails = function (email){
-            console.log("forgot details is running")
-            // console.log("The 'forgotten details' button has been activated.");
-            // document.getElementById('forgot').style.display= "inline-block";
-            // document.getElementById('signIn').style.display= "none";
-            // event.preventDefault();
-
-            // find the respective element that stores the email variable.
-            // IF myEMAIL EXISTS AND IS VALID
-            // THEN:
             signInService.remindDetails(email);
-            // myEMAIL does not exist
-
             return false;
         }
 
--- src/main/resources/public/src/main/webapp/services/service/SignIn.js
+++ src/main/resources/public/src/main/webapp/services/service/SignIn.js
@@ -5,9 +5,6 @@
         var vm = this;
 
         vm.makeNewUser = function (username, name, email, age, phone, pass) {
-            console.log("started makeNewUser 1550");
-            // var NewUser = "INSERT INTO user(Username, Password, AccessLevel) VALUES('"+username+"','"+pass+"','customer')";
-            // var NewUser = "INSERT INTO user(Username, FullName, Email, Age, Phone, Password, AccessLevel) VALUES('"+username+"','"+name+"','"+email+"','"+age+"','"+phone+"','"+pass+"','customer')";
             var NewUser = {
                 "Username": username,
                 "FullName": name,
@@ -17,21 +14,7 @@
                 "Password": pass,
                 "AccessLevel": 'customer'
             };
-            console.log(NewUser);
-            // console.log(document.getElementById('regUsername').value);
-            console.log("about to start sendUserDAta");
-            // filmDal.sendUserData('http://localhost:8081/CreateUser',NewUser
-            // ).then(function(result)
-            // {
-            //     console.log("POST RESULT: " + result.data);
-            //     console.log(JSON.stringify(result.data));
-            //     document.getElementById('regSuccess').style.display= "inline-block";
-            //     document.getElementById('register').style.display= "none";
-            // });
 
-            // NewUser=null;
-
-            // THIS WORKS FOR THE LOCAL HOST
             fetch('http://localhost:8081/CreateUser', {
                 method: 'POST', mode: 'cors', body: JSON.stringify(NewUser)
             }).then(function successCallBack(response) {
@@ -47,42 +30,10 @@
             NewUser = null;
         };
 
-        // Please resolve ARRAY_OF_USER_EMAILS and ARRAY_OF_USER_PASSWORDS, no clear context.
-        // realSignIn(var) does not appear to be linked to any real functionality.
         vm.attemptToSignIn = function (username, pass) {
-            // var OldUser= {
-            //     "Username": username,
-            //     "Password": pass
-            // };
-            // getFunction();
-            // $http.get({
-            //     // method: 'GET',
-            //     url: 'http://localhost:8081/SignIn?Username='+username+'&Password='+pass,
-            //     // body: JSON.stringify(OldUser)
-            // }).then(function successCallback(result) {
-            //     // this callback will be called asynchronously
-            //     // when the response is available
-            //     console.log("GET RESULT: " + result.data);
-            //     console.log(JSON.stringify(result.data));
-            //     document.getElementById('successSign').style.display= "inline-block";
-            //     document.getElementById('signIn').style.display= "none";
-            // }, function errorCallback(response) {
-            //     alert("Failed to Sign in");
-            //     console.log(response);
-            // });
-
-            // filmDal.signMeIn('http://localhost:8081/SignIn',OldUser
-            // ).then(function(result)
-            // {
-            //     console.log("GET RESULT: " + result.data);
-            //     console.log(JSON.stringify(result.data));
-            //     document.getElementById('successSign').style.display= "inline-block";
-            //     document.getElementById('signIn').style.display= "none";
-            // });
 
             $http.get('http://localhost:8081/SignIn?Username=' + username + '&Password=' + pass).then(function successCallback(result) {
-                // this callback will be called asynchronously
-                // when the response is available
+
                 if (result.data[0]) {
 
                     console.log("GET RESULT: " + result.data);
@@ -108,29 +59,12 @@
 
             });
 
-            // fetch('http://localhost:8081/SignIn?Username='+username+'&Password='+pass, {
-            //     method: 'GET', mode: 'cors'
-            //     // body: JSON.stringify(OldUser)
-            // }).then(function successCallBack(response) {
-            //     console.log(response.data);
-            //     //console.log(OldUser);
-            //     //console.log(JSON.stringify(OldUser));
-            //     document.getElementById('successSign').style.display= "inline-block";
-            //     document.getElementById('signIn').style.display= "none";
-            // } , function errorCallback(response) {
-            //     alert("Failed to Sign in");
-            //     console.log(response);
-            //     });
-            // OldUser=null;
 
         };
 
-        // Please resolve ARRAY_OF_USER_EMAILS, no clear context.
-        // SEND_EMAIL_TO(var) does not appear to be linked to any real functionality.
+
         vm.remindDetails = function (email) {
             $http.get('http://localhost:8081/ForgotPassword?Email=' + email).then(function successCallback(result) {
-                // this callback will be called asynchronously
-                // when the response is available
                 if (result.data[0]) {
 
                     console.log("GET RESULT: " + result.data);
@@ -151,7 +85,6 @@
                     document.getElementById('showMeAccessLevel').innerHTML += result.data[0].AccessLevel;
                 } else {
                     alert("Failed to Sign in");
-                    console.log(result.data + "didnt work");
                 }
 
             })
