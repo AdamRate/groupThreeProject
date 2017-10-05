125cff56bee5084d0d34966eda5daea9599ada44
Alan <MorickClive@Gmail.com>
2017-10-05 10:14:51 +0100

    Purchase ticket improvements
    Added sample film details for the film you'd be buying.
    Adjusted a few css cursor issues.

--- src/main/resources/public/src/main/webapp/app/feature/price/PriceController.js
+++ src/main/resources/public/src/main/webapp/app/feature/price/PriceController.js
@@ -1,9 +1,37 @@
 (function() {
 
-    var PriceController =  function() {
+    var PriceController =  function(filmLoader, $sce) {
         var vm = this;
+        
+        vm.filmDat = [];
+
+        // load film data
+        var promise = filmLoader["getFilms"]();
+        promise.then(function (data) {
+            ////
+            // When film data loaded, perform this.
+            ////
+
+            // load our specific data by movie value stored in service.
+            vm.filmDat = data[filmLoader["filmNo"]];
+            //format array string to singular string.
+            vm.castList = (function () {
+                var str = "";
+                for (var x = 0; x < vm.filmDat["castList"].length; x++) {
+                    if (x === vm.filmDat["castList"].length - 1) {
+                        str += vm.filmDat["castList"][x] + ".";
+                    }
+                    else {
+                        str += vm.filmDat["castList"][x] + ", ";
+                    }
+                }
+                return str;
+            })();
+            // consider security for videos
+            vm.videoEmbed = $sce.trustAsResourceUrl(vm.filmDat["trailer"]);
+        });        
     };
 
-    angular.module("cinema").controller("PriceController", [PriceController]);
+    angular.module("cinema").controller("PriceController", ["filmLoader", "$sce", PriceController]);
 }());
 
